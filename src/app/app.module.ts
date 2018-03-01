5a2957d11980598e18952e6acd30286107ec4079
Joseph Sungpil Choi <chousemath@gmail.com>
2018-03-01 13:01:43 +0900

    Add voice recognition

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
@@ -10,6 +15,18 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Media } from '@ionic-native/media';
+import { File } from '@ionic-native/file';
+import { SpeechRecognition } from '@ionic-native/speech-recognition';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCj-8wv8PSKfywJ9XgU1dPU4ArrGQwxXF4",
+  authDomain: "amp-test-2e29e.firebaseapp.com",
+  databaseURL: "https://amp-test-2e29e.firebaseio.com",
+  projectId: "amp-test-2e29e",
+  storageBucket: "amp-test-2e29e.appspot.com",
+  messagingSenderId: "1066201318925"
+};
 
 @NgModule({
   declarations: [
@@ -21,7 +38,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireStorageModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -34,6 +55,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    Media,
+    File,
+    SpeechRecognition,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,106 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { File } from '@ionic-native/file';
+import { SpeechRecognition } from '@ionic-native/speech-recognition';
+import { Media, MediaObject } from '@ionic-native/media';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireStorage } from 'angularfire2/storage';
+import { storage } from 'firebase';
+import * as moment from 'moment';
+import { Subscription } from 'rxjs/Subscription';
+
+declare let cordova: any;
+
+const fileName: string = 'record.3gp';
+const bucket: string = 'gs://amp-test-2e29e.appspot.com/recordings/';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  audioFile: MediaObject;
+  filePath: string;
+  speechSubscription: Subscription;
+  constructor(
+    public navCtrl: NavController,
+    public file: File,
+    public media: Media,
+    public db: AngularFireDatabase,
+    public storage: AngularFireStorage,
+    public speechRecognition: SpeechRecognition
+  ) {
+  }
 
-  constructor(public navCtrl: NavController) {
+  ionViewDidLoad() {
+    if (cordova && cordova.file && cordova.file.externalDataDirectory) {
+      this.filePath = cordova.file.externalDataDirectory.replace(/file:\/\//g, '');
+      console.log('path', this.filePath);
+    }
+    console.log(this.filePath);
+    // Check feature available
+    this.speechRecognition
+      .isRecognitionAvailable()
+      .then((available: boolean) => {
+        if (available) {
+          this.speechRecognition
+            .requestPermission()
+            .then(
+              () => console.log('Granted'),
+              () => console.log('Denied')
+            )
+        }
+      })
+  }
 
+  createSession() {
+    console.log('new session created');
+  }
+
+  record() {
+    this.speechSubscription = this.speechRecognition
+      .startListening({ language: 'en-US', showPopup: true })
+      .subscribe(
+        (matches: Array<string>) => console.log(matches[0]),
+        (onerror) => console.log('error:', onerror)
+      );
+  }
+
+  transcribe() {
+    // The name of the audio file to transcribe
+    // Reads a local audio file and converts it to base64
+    console.log('----------------');
+    console.log(this.filePath + fileName);
+    console.log('----------------');
+    this.file.readAsDataURL(cordova.file.externalDataDirectory, fileName).then(audioBytes => {
+      let audioBlob = this.uriToBlob(audioBytes)
+      let metadata = { 'contentType': audioBlob.type };
+      const key = moment().unix();
+      let ref = storage().ref(`recordings/${key}.3gp`);
+
+      ref.put(audioBlob, metadata)
+        .then(snapshot => ref.getDownloadURL())
+        .then(url => {
+          console.log(bucket + key + '.3gp');
+          console.log(url);
+          this.db.object(`recordings/${key}`).update({
+            key: key,
+            bucket: bucket + key + '.3gp',
+            url: url
+          }).then(() => console.log('update successful')).catch(err => console.log(err));
+        })
+        .catch(err => console.log(err));
+
+    }).catch(err => console.log(err));
+  }
+
+  // Function to convert dataURI to Blob needed by Firebase
+  private uriToBlob(dataURI) {
+    const dataArr = dataURI.split(',');
+    let binary = atob(dataArr[1]);
+    let array = [];
+    for (let i = 0; i < binary.length; i++) array.push(binary.charCodeAt(i));
+    return new Blob([new Uint8Array(array)], { type: 'audio/3gpp' });
   }
 
 }
